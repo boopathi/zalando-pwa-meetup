d38a76c6f1b13f719629ec353deacc7a4343d265
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2016-06-01 16:18:01 +0200

    Capability

---
+++ src/capability.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  List,
+  ListItem,
+  Slide
+} from 'spectacle';
+
+export default <Slide transition={['slide']}>
+  <List>
+    <ListItem>Installable</ListItem>
+    <ListItem>Offline</ListItem>
+    <ListItem>Safe - HTTPS</ListItem>
+    <ListItem>Performant</ListItem>
+    <ListItem>Re-Engagement</ListItem>
+</List>
+</Slide>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {render} from 'react-dom';
 import {Spectacle, Deck} from 'spectacle';
 
 import theme from './theme';
 
 import Title from './title';
+import Capability from './capability';
 import {Pass} from './fetch';
 import {Thankyou} from './trivial';
 
 const slidesOrder = [
   Title,
+  Capability,
   Pass,
   Thankyou
 ];
 
-class Show extends React.Component {
+class Show extends Component {
   render() {
     return <Spectacle theme={theme}>
       <Deck

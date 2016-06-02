f17be85894ac52c440b1700bd79b8919345c0f1c
Boopathi Rajaa <me@boopathi.in>
2016-06-02 16:05:31 +0200

    Add short-vars for es6

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 const fs = require('fs');
 const uglify = require('gulp-uglify');
 const util = require('gulp-util');
+const through2 = require('through2');
+const babel = require('babel-core');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const webpackConfig = require('./webpack.config');
@@ -27,7 +29,7 @@ if (DEV) {
   gulp.task('build', ['webpack']);
   gulp.task('watch', ['webpack-dev-server']);
 } else {
-  gulp.task('build', ['min', 'copy-app-js']);
+  gulp.task('build', ['print-size']);
 }
 
 gulp.task('webpack', cb => {
@@ -74,8 +76,37 @@ gulp.task('min', ['webpack'], function() {
     .pipe(gulp.dest('build'))
 });
 
-gulp.task('copy-app-js', ['webpack'], cb => {
-  fs.createReadStream('./build/out/app.bundle.js')
-    .pipe(fs.createWriteStream('./build/app.bundle.js'))
-    .on('end', cb);
+gulp.task('copy-app-js', ['webpack'], function () {
+  return fs.createReadStream('./build/out/app.bundle.js')
+    .pipe(through2(function(chunk, enc, callback) {
+      this.push(babel.transform(chunk, {
+        comments: false,
+        babelrc: false,
+        compact: true,
+        minified: true,
+        plugins: ['babel-plugin-short-identifiers']
+      }).code);
+      callback();
+    }))
+    .pipe(fs.createWriteStream('./build/app.bundle.js'));
 });
+
+gulp.task('print-size', ['min', 'copy-app-js'], function (cb) {
+  function formatBytes(bytes, decimals) {
+    if(bytes == 0) return '0 Byte';
+    const k = 1000;
+    const dm = decimals + 1 || 3;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  }
+
+  const vendor = formatBytes(fs.statSync('build/vendor.bundle.js').size);
+  const app = formatBytes(fs.statSync('build/app.bundle.js').size);
+
+  util.log('[Final Bundle]', `
+vendor.bundle.js: ${vendor}
+   app.bundle.js: ${app}`);
+
+  cb();
+})

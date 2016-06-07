5f5ac772c16961a03259b30b237314f18b538c5d
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2016-06-07 15:14:19 +0200

    Some changes

--- src/browsers.js
+++ src/browsers.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   List,
+  Link,
   ListItem,
   Heading,
   Slide
@@ -12,7 +13,15 @@ export default <Slide transition={['slide']}>
     <ListItem>Chrome - 45+</ListItem>
     <ListItem>Firefox - 44+</ListItem>
     <ListItem>Opera - 37+</ListItem>
-    <ListItem>Edge - In Development</ListItem>
-    <ListItem>Safari - Under consideration</ListItem>
+    <ListItem>Edge - 
+      <Link className='link' href="https://developer.microsoft.com/en-us/microsoft-edge/platform/status/serviceworker">
+        In Development
+      </Link>
+    </ListItem>
+    <ListItem>Safari - 
+      <Link className='link' href="https://webkit.org/status/#specification-service-workers">
+        Under consideration
+      </Link>
+    </ListItem>
 </List>
 </Slide>

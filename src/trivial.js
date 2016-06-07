b8de791bb764a2dce3564edba17b5daa0eedbedc
Boopathi Rajaa <me@boopathi.in>
2016-06-07 15:42:10 +0200

    Trivial

--- src/trivial.js
+++ src/trivial.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Slide, Heading, Text} from 'spectacle';
 
-export const Thankyou = <Slide bgColor='#795548' transition={['slide']}>
+export const Thankyou = <Slide bgColor='#9C27B0' transition={['slide']}>
   <Heading>Thank you</Heading>
   <Text lineHeight={3}>Questions/Discussions</Text>
 </Slide>

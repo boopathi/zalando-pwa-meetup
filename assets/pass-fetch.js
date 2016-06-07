314b75957d16fbd1cf3cdf0bc80f41c3a8c5702d
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2016-06-07 11:39:01 +0200

    Adding link

--- assets/pass-fetch.js
+++ assets/pass-fetch.js
@@ -6,37 +6,3 @@ self.addEventListener(
     );
   }
 );
-asdlkfj
-
-
-self.addEventListener(
-  'fetch',
-  function (event) {
-    event.respondWith(
-      fetch(event.request)
-    );
-  }
-);
-asdlkfj
-
-
-self.addEventListener(
-  'fetch',
-  function (event) {
-    event.respondWith(
-      fetch(event.request)
-    );
-  }
-);
-asdlkfj
-
-
-self.addEventListener(
-  'fetch',
-  function (event) {
-    event.respondWith(
-      fetch(event.request)
-    );
-  }
-);
-asdlkfj
--- src/future.js
+++ src/future.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {Slide, Heading, List, ListItem} from 'spectacle';
+import {Slide, Heading, Link, List, ListItem} from 'spectacle';
 
 export default <Slide transition={['slide']}>
-  <Heading size={4}>Whats Missing?</Heading>
+  <Heading lineHeight={2} size={4}>Whats Missing?</Heading>
   <List>
     <ListItem>Differentiate PWA from existing websites</ListItem>
     <ListItem>Control 'Add to Homescreen' Prompt(overkill)</ListItem>
+    <ListItem>Ambient Badging</ListItem>
   </List>
+  <Link className='link' textSize={22} href="https://infrequently.org/2016/06/pwa-discovery-you-aint-seen-nothin-yet/">PWA Discovery by Alex Russell</Link>
 </Slide>
